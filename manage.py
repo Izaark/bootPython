3f023e0bada4fb9fbd05d67127e7516407ec456a
Isaac López <izaak00@hotmail.com>
2017-01-20 22:11:44 -0600

    get messages

--- manage.py
+++ manage.py
@@ -1,12 +1,34 @@
 from flask import Flask
-from config import DevelopmentConfig 
+from flask import request
+
+import json
+from config import DevelopmentConfig
+
 
 app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
+app.config.from_object( DevelopmentConfig )
 
-@app.route('/',methods =['GET'])
+@app.route('/webhook', methods = ['GET', 'POST'])
+def webhook():
+	if request.method == 'GET':
+		verify_token = request.args.get('hub.verify_token', '')
+		if verify_token == app.config['SECRET_KEY']:
+			return request.args.get('hub.challenge', '')
+		return 'Error al validar el secreto'
+
+	elif request.method == 'POST':
+		payload  = request.get_data()
+		data = json.loads(payload)
+
+		for page_entry in data['entry']:
+			for message_event in page_entry['messaging']:
+					evento = message_event['message']
+					print evento['text']
+
+		return "ok"
+
+@app.route('/', methods = ['GET'])
 def index():
-	return 'Hello world'
-	
+	return 'Hola al curso de Bot!'
 if __name__ == '__main__':
-	app.run(port=8000)
+	app.run(port = 8000)
